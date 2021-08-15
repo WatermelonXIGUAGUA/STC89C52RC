96c603f83586a42ec7981339e600264165a3a549
WatermelonXIGUAGUA <zengweiwymail@163.com>
2021-08-15 15:18:07 +0800

    over

--- 定时器时钟/main.c
+++ 定时器时钟/main.c
@@ -4,7 +4,7 @@
 #include "INTRINS.h"
 #include "LCD1602.h"
 
-unsigned char Sec;
+unsigned char Sec, Min, Hour;
 
 void main()
 {
@@ -15,19 +15,34 @@ void main()
 
 	while (1)
 	{
-		LCD_ShowNum(2,1,Sec,5)
-	}
+		LCD_ShowNum(2, 1, Hour, 2);
+		LCD_ShowString(2,3,":");
+		LCD_ShowNum(2, 4, Min, 2);
+		LCD_ShowString(2,6,":");
+		LCD_ShowNum(2, 7, Sec, 2);
+	} 
+}
 
-	void Timer0_Routine() interrupt 1
+void Timer0_Routine() interrupt 1
+{
+	static unsigned int T0Count;
+	TL0 = 0x18; //设置定时初值
+	TH0 = 0xFC; //设置定时初值
+	T0Count++;
+	if (T0Count >= 10)
 	{
-		static unsigned int T0Count;
-		TL0 = 0x18; //设置定时初值
-		TH0 = 0xFC; //设置定时初值
-		T0Count++;
-		if (T0Count >= 1000)
+		T0Count = 0;
+		if (Sec++ >= 60)
 		{
-			T0Count = 0;
-			Sec++;
+			Sec = 0;
+			if (Min++ >= 60)
+			{
+				Min = 0;
+				if (Hour++ >= 24)
+				{
+					Hour = 0;
+				}
+			}
 		}
 	}
 }
